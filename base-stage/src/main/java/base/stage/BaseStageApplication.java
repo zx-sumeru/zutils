a9c56074e4f47fab7a237903179393a6af3c09f8
Zhang Xu <1243399327@qq.com>
2018-10-04 18:26:32 +0800

    添加一个基础脚手架模块

---
+++ base-stage/src/main/java/base/stage/BaseStageApplication.java
@@ -0,0 +1,12 @@
+package base.stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseStageApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zx/validator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zx
+ * @date 2018/10/4 下午5:34
+ */
+package com.zx.validator;
